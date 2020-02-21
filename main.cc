41e6f76ed36ca72d0bbde9c20fe665c1e24118ee
Mona Lisa <troyappel@college.harvard.edu>
2020-02-21 18:04:01 -0500

    heapify finished

--- main.cc
+++ main.cc
@@ -1,21 +1,64 @@
 #include <iostream>
+#include <unordered_map>
 
 int main() {
     std::cout << "Hello World!";
 }
 
-template<typename I, typename J>
+
+struct heaptype {
+    double weight;
+    void* ptr;
+};
+
 struct heap{
-    int array[];
+    heaptype* arr; // Array is one bigger; let first element start at one
+    size_t max_size;
+
+    size_t used_size;
+
+    std::unordered_map<void*, size_t> m;
 
     heap(size_t n) {
-        array = malloc(n);
+        arr = (heaptype*) malloc(sizeof(heaptype) * (n+1));
+        max_size = n;
     };
     ~heap() {
-        free(array);
+        free(arr);
     };
 
-    J peek();
-    J remove();
-    J remove_and_add(I i);
+    inline void swap_els(heaptype* a, heaptype* b) {
+        heaptype temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    heaptype peek() {
+
+    };
+
+    void heapify() {
+        size_t i = 1;
+        while (true) {
+            if(arr[i].weight > arr[2*i].weight) {
+                swap_els(&arr[i], &arr[2*i]);
+                i = 2*i;
+                continue;
+            } else if (arr[i].weight > arr[2*i+1].weight) {
+                swap_els(&arr[i], &arr[2*i + 1]);
+                i = 2*i + 1;
+                continue;
+            }
+
+            break;
+
+        }
+    }
+
+    heaptype remove();
+    heaptype remove_and_add(heaptype i);
+    void add(heaptype i);
+    void alter(void* ptr, double weight) {
+
+    };
 };
